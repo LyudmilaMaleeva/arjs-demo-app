59f8e63635db7cf3da576d78b1ba53ca58598d26
Eugene Olisevich <olisevich@realtimeboard.com>
2018-07-27 09:24:57 +0500

    Meeting roof update

--- scripts/meeting-room.js
+++ scripts/meeting-room.js
@@ -1,25 +1,36 @@
-function activateMeetingRoom(buttonId) {
-	var calendrBack = document.querySelector('.calendar-back');
-	var cancelEventBtn = document.querySelector('.cancelEvent');
-	var addEventBtn = document.querySelector('.addEvent');
-	var eventNameInput = document.querySelector('.eventNameInput');
+function activateMeetingRoom(buttonId, upBtnId) {
+    var el;
+    var calendrBack = document.querySelector('.calendar-back');
+    var cancelEventBtn = document.querySelector('.cancelEvent');
+    var addEventBtn = document.querySelector('.addEvent');
+    var eventNameInput = document.querySelector('.eventNameInput');
 
-	var el = document.getElementById(buttonId);
+    var dragStartListener = function (evt) {
+        if (calendrBack.classList.contains('hidden')) {
+            el = evt.target;
+            eventNameInput.value = '';
+            calendrBack.classList.toggle('hidden');
+            eventNameInput.focus();
+        }
+    };
 
-	el.addEventListener('dragstart', function (evt) {
-		eventNameInput.value = '';
-		calendrBack.classList.toggle('hidden');
-		eventNameInput.focus();
-	});
-	cancelEventBtn.addEventListener('click', function () {
-		calendrBack.classList.toggle('hidden');
-	});
-	addEventBtn.addEventListener('click', function() {
-		var a = el.getAttribute('text');
-		el.setAttribute('text', 'value: ' + eventNameInput.value);
-		el.removeAttribute('material');
-		el.removeAttribute('geometry');
-		el.flushToDOM(true);
-		calendrBack.classList.toggle('hidden');
-	});
+    cancelEventBtn.addEventListener('click', function () {
+        calendrBack.classList.toggle('hidden');
+    });
+    addEventBtn.addEventListener('click', function (event) {
+        el.setAttribute('text', 'value: ' + eventNameInput.value);
+        el.setAttribute('text', 'color: ' + '#fff');
+        el.removeAttribute('material');
+        el.removeAttribute('geometry');
+        el.flushToDOM(true);
+        calendrBack.classList.toggle('hidden');
+    });
+
+    window.step1 = function(visible) {
+      if (visible) {
+        document.addEventListener('dragstart', dragStartListener);
+      } else {
+        document.removeEventListener('dragstart', dragStartListener);
+      }
+    }
 }
